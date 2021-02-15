1f56e9b6c7ba00c050a0a0bbbb436cd6f9a27b86
IlScottlI <scotteverettjohnson84@gmail.com>
2021-02-15 12:23:17 -0500

    Zoo Keeper: Part Two

--- OOP/003_Zookeeper/Zookeeper/src/TestFile.java
+++ OOP/003_Zookeeper/Zookeeper/src/TestFile.java
@@ -3,12 +3,23 @@ public class TestFile {
 
 	public static void main(String[] args) {
 		Gorilla g = new Gorilla();
+		Bat b = new Bat();
 		g.throwSomething();
 		g.throwSomething();
 		g.throwSomething();
 		g.eatBananas();
 		g.eatBananas();
 		g.climb();
+		b.fly();
+		b.fly();
+		b.displayEnergy();
+		b.eatHumans();
+		b.eatHumans();
+		b.displayEnergy();
+		b.attackTown();
+		b.attackTown();
+		b.attackTown();
+		b.displayEnergy();
 	}
 
 }
