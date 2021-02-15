7680f9f6b1646e0b15a1888f58ff376e64ab3cbf
IlScottlI <scotteverettjohnson84@gmail.com>
2021-02-15 15:22:55 -0500

    Object Master: Part One

---
+++ OOP/ObjectMater/src/HumanTest.java
@@ -0,0 +1,9 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human h1 = new Human("Scott");
+        Human h2 = new Human("Dan");
+        
+        h2.attack(h1);
+
+    }
+}
